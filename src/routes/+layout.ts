6fb36aa66323c8432d2243a907e51ff6256926c4
Cory Hayes <corymhayes@gmail.com>
2024-11-13 21:20:13 -0600

    added static adapter

---
+++ src/routes/+layout.ts
@@ -0,0 +1,4 @@
+export const ssr = false;
+export const prerender = true;
+
+// seeking a role as a software devleoper with skillset in ____. Passionate about software engineering and web design and commited to deligiving ______.
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: "200.html",
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
